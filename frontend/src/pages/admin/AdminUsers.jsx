95f03daa3c9b85f81c756edfa9332bfc338cfb64
TraptiSingh7300 <traptisingh7300@gmail.com>
2026-04-30 15:05:14 +0530

    Created Product page in admin dashboard

--- frontend/src/pages/admin/AdminUsers.jsx
+++ frontend/src/pages/admin/AdminUsers.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Input } from '@/components/ui/input'
+import axios from 'axios'
+import { Edit, Eye, Search } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import UserLogo from "../../assets/user.png"
+import { Button } from '@/components/ui/button'
 
 const AdminUsers = () => {
-  return (
+  return(
     <div>AdminUsers</div>
   )
 }
